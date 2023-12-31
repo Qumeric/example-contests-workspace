59c376e3b01f814a0c64a975ced687220fa4a08c
MetaB0y <qumidium@gmail.com>
2023-12-31 10:57:54 +0000

    Add easy task

---
+++ b_two_divisors/src/main.rs
@@ -0,0 +1,73 @@
+//{"name":"B. Two Divisors","group":"Codeforces - Good Bye 2023","url":"https://codeforces.com/contest/1916/problem/B","interactive":false,"timeLimit":1000,"tests":[{"input":"8\n2 3\n1 2\n3 11\n1 5\n5 10\n4 6\n3 9\n250000000 500000000\n","output":"6\n4\n33\n25\n20\n12\n27\n1000000000\n"}],"testType":"single","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"BTwoDivisors"}}}
+
+use std::collections::{BTreeSet, HashSet};
+
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+use algo_lib::math::{factorize, factorize_map};
+use algo_lib::numbers::gcd::gcd;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &PreCalc) {
+    let a: i64 = input.read();
+    let b: i64 = input.read();
+
+    // x / b = SMALLEST_PRIME
+    // x / a = SMALL_PRIME or SMALLEST_PRIME ^ 2
+
+    let fa = factorize_map(a);
+    let fb = factorize_map(b);
+
+    let mut difs: Vec<i64> = vec![];
+
+    let all_keys: BTreeSet<i64> = fa.keys().cloned().chain(fb.keys().cloned()).collect();
+
+    for k in all_keys.iter() {
+        if fa.get(k) != fb.get(k) {
+            difs.push(*k);
+        }
+    }
+
+    // out.print_line(difs);
+    out.print_line(b * difs[0]);
+}
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    let pre_calc = ();
+
+    #[allow(dead_code)]
+    enum TestType {
+        Single,
+        MultiNumber,
+        MultiEof,
+    }
+    let test_type = TestType::MultiNumber;
+    match test_type {
+        TestType::Single => solve(&mut input, &mut output, 1, &pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 0usize..t {
+                solve(&mut input, &mut output, i + 1, &pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &pre_calc);
+                i += 1;
+            }
+        }
+    }
+    output.flush();
+    input.skip_whitespace();
+    input.peek().is_none()
+}
+
+//START MAIN
+mod tester;
+
+fn main() {
+    tester::run_tests();
+}
+//END MAIN
